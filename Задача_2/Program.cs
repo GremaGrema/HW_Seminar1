c44bae54178b98175543c4ba3eb701c8e1743fab
Евгений Долматов <C:\Users\datat\AppData\Roaming\The Bat!>
2022-08-11 20:18:57 +0700

    задача_2

---
+++ Задача_2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write ("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write ("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine());
+if (numberA == numberB)
+Console.Write($"Числа {numberB} и {numberA} равны");
+else
+if (numberA > numberB)
+Console.Write($"Число {numberA} больше, а число {numberB} меньше");
+else
+Console.Write($"Число {numberB} больше, а число {numberA} меньше");
