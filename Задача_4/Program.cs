c214fcbce302d8a376b91d9b564240f1e3c62f31
Евгений Долматов <C:\Users\datat\AppData\Roaming\The Bat!>
2022-08-14 00:49:12 +0700

    Задачи

--- Задача_4/Program.cs
+++ Задача_4/Program.cs
@@ -1,8 +1,25 @@
-﻿ int i = 0;
-            int n = Convert.ToInt32(textBox1.Text);
-            int m = Convert.ToInt32(textBox2.Text);
-            for (i = n; i >= m; i++)
-            {
-                if (i % 2 == 0)
-                {
-                    textBox3.Text += Convert.ToString(i);
+﻿Console.Write ("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write ("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine());
+Console.Write ("Введите второе число: ");
+int numberC = int.Parse(Console.ReadLine());
+
+if (numberA == numberB)
+{
+    if (numberC == numberB)
+    Console.Write("Числа равны");
+}
+else
+    if (numberA > numberB)
+    {
+            if (numberA > numberC)
+            Console.Write($"Число {numberA} больше");
+    }
+else
+    if (numberB > numberC)
+    {
+                Console.Write($"Число {numberB} больше");
+    }
+    else            
+    Console.Write($"Число {numberC} больше");
